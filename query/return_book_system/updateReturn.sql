13f8995174dbf9e075655d015a1c49e7bdb13e39
ytlau <90511311+YTlau9487@users.noreply.github.com>
2025-11-29 15:47:40 +0800

    submit the book return queries

---
+++ query/return_book_system/updateReturn.sql
@@ -0,0 +1,12 @@
+# update the loan_records.ret_date and books_location.book_status = ‘O’
+UPDATE
+  books_location AS bl
+JOIN 
+  oan_records AS lr
+ON
+  (lr.books_location_id = bl.books_location_id)
+SET
+  lr.ret_date = CURDATE(),
+  bl.book_status = 'O'
+WHERE
+  lr.loan_records_id = :lrId;
