aa871b6768367cc5d8fd6ec3e8e65446b2509755
ytlau <90511311+YTlau9487@users.noreply.github.com>
2025-12-03 14:25:22 +0800

    finished the return book system (both frontend and backend) (#19)

--- flask_app/routes/returnBook.py
+++ flask_app/routes/returnBook.py
@@ -1,29 +1,115 @@
-from flask import Blueprint, render_template, request
+from flask import Blueprint, render_template, request, redirect, url_for
 from sqlalchemy import text
 from ..utils.database import engine
 from ..db.return_book_system.isRecorded import isRecorded
 from ..db.return_book_system.displayReaderName import displayReaderName
 from ..db.return_book_system.displayNotReturn import displayNotReturn
+from ..db.return_book_system.returnBookByLoanID import returnBookByLoanID
+from ..db.return_book_system.getReaderIdByLoanID import getReaderIdByLoanID
 
 return_book_bp = Blueprint("returnBook", __name__)
 
-@return_book_bp.route("/return-book", methods=["GET","POST"])
+# @return_book_bp.route("/return-book", methods=["GET","POST"])
+# def return_book():
+#   status = request.args.get("status")
+#   reader_id_qs = request.args.get("reader_id")
+
+#   if request.method == "GET":
+#     return render_template("returnBook.html")
+  
+#   reader_id = request.form.get("reader_id")
+  
+#   try:
+#     reader_id = int(reader_id)
+#   except:
+#     error = "Invalid reader ID"
+#     return render_template("returnBook.html",error=error)
+  
+#   if isRecorded(engine, reader_id) == 1:
+#     reader_name = displayReaderName(engine, reader_id)
+#     loans_books = displayNotReturn(engine, reader_id)
+
+#     sucess_msg = None
+#     error_msg = None
+#     if status == "success":
+#       success_msg = "Book returned successfully"
+#     elif status -- "error":
+#       error_msg = "Error occurred when returning book"
+#     return render_template("returnBook.html",reader_name=reader_name, loans_books=loans_books, success=success_msg, error=error_msg)
+#   else:
+#     error = "Reader does not exist"
+#     return render_template("returnBook.html",error=error)
+
+@return_book_bp.route("/return-book", methods=["GET", "POST"])
 def return_book():
-  if request.method == "GET":
-    return render_template("returnBook.html")
+    status = request.args.get("status")
+    reader_id_qs = request.args.get("reader_id")
+
+    if request.method == "GET" and reader_id_qs:
+        try:
+            reader_id = int(reader_id_qs)
+        except:
+            return render_template("returnBook.html", error="Invalid reader ID")
+
+        if isRecorded(engine, reader_id) == 1:
+            reader_name = displayReaderName(engine, reader_id)
+            loans_books = displayNotReturn(engine, reader_id)
+
+            success_msg = None
+            error_msg = None
+            if status == "success":
+                success_msg = "Book returned successfully."
+            elif status == "error":
+                error_msg = "Error occurred when returning book."
+
+            return render_template(
+                "returnBook.html",
+                reader_name=reader_name,
+                loan_books=loans_books,
+                success=success_msg,
+                error=error_msg,
+            )
+        else:
+            return render_template("returnBook.html", error="Reader does not exist")
+
+    if request.method == "GET":
+        return render_template("returnBook.html")
+
+    # Post: user search reader id
+    reader_id = request.form.get("reader_id")
+
+    try:
+        reader_id = int(reader_id)
+    except:
+        error = "Invalid reader ID"
+        return render_template("returnBook.html", error=error)
+
+    if isRecorded(engine, reader_id) == 1:
+        reader_name = displayReaderName(engine, reader_id)
+        loans_books = displayNotReturn(engine, reader_id)
+        return render_template(
+            "returnBook.html",
+            reader_name=reader_name,
+            loan_books=loans_books,
+        )
+    else:
+        error = "Reader does not exist"
+        return render_template("returnBook.html", error=error)
   
-  reader_id = request.form.get("reader_id")
-  
+
+@return_book_bp.route("/return-book/confirm", methods=["POST"])
+def confirm_return():
+  loan_record_id = request.form.get("loan_record_id")
+
   try:
-    reader_id = int(reader_id)
+      loan_record_id = int(loan_record_id)
   except:
-    error = "Invalid reader ID"
-    return render_template("returnBook.html",error=error)
+      return redirect(url_for("returnBook.return_book",status="error"))
+
+  returnStatus = returnBookByLoanID(engine, loan_record_id)
+
+  if not returnStatus:
+     return redirect(url_for("returnBook.return_book",status="error"))
   
-  if isRecorded(engine, reader_id) == 1:
-     reader_name = displayReaderName(engine, reader_id)
-     loans_books = displayNotReturn(engine, reader_id)
-     return render_template("returnBook.html", reader_name=reader_name, loan_books=loans_books)
-  else:
-    error = "Reader does not exist"
-    return render_template("returnBook.html",error=error)
+  reader_id = getReaderIdByLoanID(engine, loan_record_id)
+  return redirect(url_for("returnBook.return_book",reader_id=reader_id, status="success"))
